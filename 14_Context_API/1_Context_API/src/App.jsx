65123abeac676572dde6b5670ac0d047b49b81ad
Tejas9420190282 <96009244+Tejas9420190282@users.noreply.github.com>
2024-08-14 19:19:14 +0530

    a

--- 14_Context_API/1_Context_API/src/App.jsx
+++ 14_Context_API/1_Context_API/src/App.jsx
@@ -1,16 +1,14 @@
 import { createContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import Comp1 from "./component/Comp1"
 
 
-{/*           1) create provider */}
+/* 1) create provider */
+
 export const  data = createContext();
 
 function App() {
-
   
   const name = "Tejas Shimpi";
   

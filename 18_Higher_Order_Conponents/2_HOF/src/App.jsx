32a145edbd443534bb802212f1da13cd286c1e8f
“Tejas9420190282” <tejasshimpi877@gmail.com>
2024-10-31 16:10:17 +0530

    Higher order component

--- 18_Higher_Order_Conponents/2_HOF/src/App.jsx
+++ 18_Higher_Order_Conponents/2_HOF/src/App.jsx
@@ -2,19 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Counter from './components/Counter'
-import HOF from './components/HOF'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <HOF cmp={Counter} clr={"red"} />
 
-        <HOF cmp={Counter} clr={"blue"} />
-
-        <HOF cmp={Counter} clr={"skyblue"} />
     </>
   )
 }
